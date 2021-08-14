ac310bff6519204758395486ddd7a7b40d6ed03c
mpilopillz <mpilo.dlamini@yahoo.com>
2021-08-14 04:41:30 +0200

    wiring up components to app js

--- complex/frontend/src/App.js
+++ complex/frontend/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import OtherPage from "./OtherPage";
+import Fib from "./Fib";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route exact path="/otherpage" component={OtherPage} />
+          <Link to="/">Home</Link>
+          <Link to="/otherpage">OtherPage</Link>
+        </div>
+      </div>
+    </Router>
   );
 }
 
--- complex/frontend/src/Fib.js
+++ complex/frontend/src/Fib.js
@@ -68,3 +68,5 @@ class Fib extends Component {
     );
   }
 }
+
+export default Fib;
