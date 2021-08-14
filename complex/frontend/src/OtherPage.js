6e57ec69f3b38a3021bac4c8fbede817532b5391
mpilopillz <mpilo.dlamini@yahoo.com>
2021-08-14 04:58:28 +0200

    adding odkerfiles and adding a variable for other page

--- complex/frontend/src/OtherPage.js
+++ complex/frontend/src/OtherPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default () => {
+const OtherPage = () => {
   return (
     <div>
       O am a page
@@ -9,3 +9,5 @@ export default () => {
     </div>
   );
 };
+
+export default OtherPage;
