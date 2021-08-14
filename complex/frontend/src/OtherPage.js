7a0706a62fb55ca0d50946960af18b26691f3780
mpilopillz <mpilo.dlamini@yahoo.com>
2021-08-14 04:22:01 +0200

    adding react boiler plate

---
+++ complex/frontend/src/OtherPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default () => {
+  return (
+    <div>
+      O am a page
+      <Link to="/">Go back home</Link>
+    </div>
+  );
+};
