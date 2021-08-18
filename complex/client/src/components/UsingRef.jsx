8ca7bfb0841582bc01cd8435cd1b6fb345aa8d5e
mpilopillz <mpilo.dlamini@yahoo.com>
2021-08-19 01:40:11 +0200

    adding useeffect prev

--- complex/client/src/components/UsingRef.jsx
+++ complex/client/src/components/UsingRef.jsx
@@ -1,8 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 export default function UsingRef() {
   const [name, setName] = useState("");
+  // const renderCount = useRef(0);
+  const [renderCount, setRenderCount] = useState(0);
 
+  useEffect(() => setRenderCount((prevRenderCount) => prevRenderCount + 1), []); //rendered infinitly without the [] with the [] it does not work
+  // useEffect(() => {
+  //   renderCount.current = renderCount.current + 1;
+  // });
   return (
     <>
       <input
@@ -11,6 +17,8 @@ export default function UsingRef() {
         type="text"
       />
       <p>My name is {name}</p>
+      <p>I have rendered {renderCount} times</p>
+      {/* <p>I have rendered {renderCount.current} times</p> */}
     </>
   );
 }
