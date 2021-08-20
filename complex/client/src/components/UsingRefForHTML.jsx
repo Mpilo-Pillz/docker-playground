2533c4b1088a8b7a9f2789089c8de95007599003
mpilopillz <mpilo.dlamini@yahoo.com>
2021-08-20 07:49:57 +0200

    exploring more use ref

---
+++ complex/client/src/components/UsingRefForHTML.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState, useRef } from "react";
+
+export default function UsingRefForHTML() {
+  const [name, setName] = useState("");
+  const inputRef = useRef();
+  const prevName = useRef("");
+
+  useEffect(() => {
+    prevName.current = name;
+  }, [name]);
+
+  function focus() {
+    console.log(inputRef.current);
+    inputRef.current.focus();
+  }
+
+  return (
+    <>
+      <input
+        ref={inputRef}
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        type="text"
+      />
+      <div>
+        My name is {name} and it used to be {prevName.current}
+      </div>
+      <button onClick={focus}>Focus</button>
+      {/* <p>I have rendered {renderCount.current} times</p> */}
+    </>
+  );
+}
