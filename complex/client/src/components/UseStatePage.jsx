e5ba6ef586a159823ffcd73bc2b3aff3823f3a88
mpilopillz <mpilo.dlamini@yahoo.com>
2021-08-26 10:09:53 +0200

    looking at how to prevent a rrender with useState

--- complex/client/src/components/UseStatePage.jsx
+++ complex/client/src/components/UseStatePage.jsx
@@ -1,7 +1,16 @@
 import React, { useState } from "react";
 
+function countInitial() {
+  console.log("initCOuntRan");
+  return 4;
+}
+function reRenders() {
+  console.log("Rerenders");
+  return 0;
+}
 const UseStatePage = () => {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(reRenders());
+  const [altCount, setAltCount] = useState(() => countInitial());
 
   function decrementCount() {
     setCount((prevCount) => prevCount - 1);
@@ -10,11 +19,22 @@ const UseStatePage = () => {
   function incrementCount() {
     setCount((prevCount) => prevCount + 1);
   }
+  function multiplyCount() {
+    setAltCount((prevCount) => prevCount * 2);
+  }
+
+  function divideCount() {
+    setAltCount((prevCount) => prevCount / 2);
+  }
   return (
     <div>
       <button onClick={decrementCount}>-</button>
       <span>{count}</span>
       <button onClick={incrementCount}>+</button>
+      <hr />
+      <button onClick={divideCount}>/</button>
+      <span>{altCount}</span>
+      <button onClick={multiplyCount}>*</button>
     </div>
   );
 };
