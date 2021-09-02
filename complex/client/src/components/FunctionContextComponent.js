293e600faad446bb921b0082b82f7d0cba07fc9e
mpilopillz <mpilo.dlamini@yahoo.com>
2021-09-02 17:33:54 +0200

    adding functional context

---
+++ complex/client/src/components/FunctionContextComponent.js
@@ -0,0 +1,14 @@
+import React, { useContext } from "react";
+import { ThemeContext } from "../App";
+
+export default function FunctionalContextComponent() {
+  const darkTheme = useContext(ThemeContext);
+
+  const themeStyles = {
+    backgroundColor: darkTheme ? "#333" : "#ccc",
+    color: darkTheme ? "#ccc" : "#333",
+    padding: "2rem",
+    margin: "2rem",
+  };
+  return <div style={themeStyles}>Function Theme</div>;
+}
